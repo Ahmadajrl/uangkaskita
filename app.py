9e4381135b97869dbf1cdfd9ec276207ff3f36ac
Ahmadajrl <143680000+Ahmadajrl@users.noreply.github.com>
2026-04-11 19:59:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -197,30 +197,6 @@ else:
         st.success(f"Kelas {st.session_state.kelas} - {st.session_state.jurusan}")
 
         colA, colB = st.columns(2)
-    elif st.session_state.role == "user":
-
-    st.subheader("📋 Data Kas Siswa")
-
-    df = pd.read_sql("SELECT * FROM kas", conn)
-
-    if not df.empty:
-        df["tanggal"] = pd.to_datetime(df["tanggal"])
-        df["bulan"] = df["tanggal"].dt.strftime("%B %Y")
-
-        col1, col2 = st.columns(2)
-
-        with col1:
-            jurusan_list = sorted(df["jurusan"].dropna().unique())
-            pilih_jurusan = st.selectbox("🎓 Pilih Jurusan", jurusan_list)
-
-        with col2:
-            bulan_list = sorted(df["bulan"].unique())
-            pilih_bulan = st.selectbox("📅 Pilih Bulan", bulan_list)
-
-        df_filter = df[
-            (df["jurusan"] == pilih_jurusan) &
-            (df["bulan"] == pilih_bulan)
-        ]
 
         if not df_filter.empty:
             df_filter["tanggal"] = df_filter["tanggal"].dt.strftime("%Y-%m-%d")
