016a7792bef28a5bb9d20bb14330b360653d6fff
Ahmadajrl <143680000+Ahmadajrl@users.noreply.github.com>
2026-04-17 23:07:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,11 +272,7 @@ if not st.session_state.login and st.session_state.page == "role":
 # LOGIN / REGISTER / FORGOT
 # ======================
 elif not st.session_state.login:
-    if st.button("Kembali"):
-        st.session_state.page = "role"
-        st.session_state.role = None
-        st.rerun()
-        
+
     st.title("Authentication")
 
     tab1, tab2, tab3 = st.tabs(["Login", "Register", "Lupa Password"])
@@ -285,6 +281,7 @@ elif not st.session_state.login:
     with tab1:
 
         if st.session_state.role == "admin":
+
             user = st.text_input("Username", key="login_user")
             pw = st.text_input("Password", type="password", key="login_pass")
             kelas = st.selectbox("Kelas", ["10","11","12"])
@@ -307,18 +304,18 @@ elif not st.session_state.login:
                 else:
                     st.error("Username atau password salah")
 
-    elif st.session_state.role == "dev":
+        elif st.session_state.role == "dev":
 
-        user = st.text_input("Username Developer")
-        pw = st.text_input("Password Developer", type="password")
+            user = st.text_input("Username Developer")
+            pw = st.text_input("Password Developer", type="password")
 
-        if st.button("Login Developer"):
-            if user == DEV_USER and pw == DEV_PASS:
-                st.session_state.login = True
-                st.session_state.role = "dev"
-                st.rerun()
-            else:
-                st.error("Login gagal")
+            if st.button("Login Developer"):
+                if user == DEV_USER and pw == DEV_PASS:
+                    st.session_state.login = True
+                    st.session_state.role = "dev"
+                    st.rerun()
+                else:
+                    st.error("Login gagal")
 
     # ================= REGISTER =================
     with tab2:
